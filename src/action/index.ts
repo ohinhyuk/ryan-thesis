b4463d59ac560ebc6469407707b617da231b7df7
오인혁 <oinhyuk@oinhyeog-ui-MacBookPro.local>
2024-04-05 16:40:26 +0900

    SSG -> SSR로 수정

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,37 +1,11 @@
-export async function onSubmit(formData: FormData) {
-  "use server";
-  // event.preventDefault();
-
-  console.log("22");
-
-  // const formData = new FormData(event.currentTarget);
-  console.log(formData.get("content"));
-
-  if (formData.get("password") !== "ryan") {
-    return;
-  }
-
-  console.log(formData.get("content"), formData.get("password"));
-  // const response = await fetch("/api/submit", {
-  //   method: "POST",
-  //   body: formData,
-  // });
-
-  console.log("@@");
-  revalidatePath("/thesis", "page");
-}
-
-import { PublicationDatabase } from "@/schema/thesis";
-import { revalidatePath } from "next/cache";
-import { headers } from "next/headers";
-
 export const getThesis = async () => {
   const getThesis = await fetch(
-    "https://api.notion.com/v1/databases/313fb755e0124aecb193e666d7d671b0/query",
+    `https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`,
     {
+      cache: "no-store",
       method: "POST",
       headers: {
-        Authorization: "secret_U2s7zPXUgYm3RrVR6ayqVbSZuwoGzvt8EdtfNtChVn1",
+        Authorization: `${process.env.NOTION_API_SECRET_KEY}`,
         "Notion-Version": "2022-02-22",
       },
     }
